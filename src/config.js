45c676c7703bf77c23ddf48508b5283bcaeeb206
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-10-04 13:42:29 +0100

    config for S3 bucket
    config for S3 bucket

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// src/config.js
+const config = {
+    S3_BUCKET_URL: 'http://reactbucketachievement1-1.7.s3-website.eu-west-2.amazonaws.com',
+};
+
+export default config;
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 // Creates root of project
+import config from './config';
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/MainView/main-view';
 import Container from 'react-bootstrap/Container';
@@ -7,11 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //Import myFlix-client.scss
 import './index.scss';
 
+const indexHtmlUrl = `${config.S3_BUCKET_URL}/dist/index.html`; 
+
 //Main component (will eventually use all the others)
 const App = () => {
     return (
         <Container>
             <MainView />
+            <a href={indexHtmlUrl}>Link to index.html</a>
         </Container>
     );
 };
